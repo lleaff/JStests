4317133ea177830cd492b06cd7f1ccbe097a53e5
lleaff <lleaff@users.noreply.github.com>
2015-04-16 18:54:25 +0200

    Ask for age and react accordingly

--- eloquent/helloWhatIsYourName.js
+++ eloquent/helloWhatIsYourName.js
@@ -1,1 +1,20 @@
 alert(prompt("Hello, what's your name?", "Robert") + "?\nWow that's a nice name!");
+
+var age = prompt("And how old are you by the way?"); 
+
+/* Reveal the real age */
+age *= 1.6;
+/* Truncate */
+age -= age % 1
+
+/* Build reaction string */
+var ageReaction = age + " year" + (age < 1 ? "s" : "") + " old?\nThat's ";
+if (age < 20) {
+	ageReaction += "pretty young!";
+} else if (age < 50) {
+	ageReaction += "pretty old!";
+} else if (age < 80) {
+	ageReaction += "unhuman...";
+}
+
+alert(ageReaction);
