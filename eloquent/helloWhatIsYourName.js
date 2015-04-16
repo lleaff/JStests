278cfccaa698f019f3224c70da01759e565e0840
lleaff <lleaff@users.noreply.github.com>
2015-04-16 19:22:36 +0200

    +loop until valid result is entered, react according to times asked

--- eloquent/helloWhatIsYourName.js
+++ eloquent/helloWhatIsYourName.js
@@ -1,20 +1,57 @@
-alert(prompt("Hello, what's your name?", "Robert") + "?\nWow that's a nice name!");
+var name = 0;
+var defaultName = "Robert";
+var timesAsked = 1;
+while (true) {
+	name = prompt("Hello, what's your name?");
 
-var age = prompt("And how old are you by the way?"); 
-
-/* Reveal the real age */
-age *= 1.6;
-/* Truncate */
-age -= age % 1
-
-/* Build reaction string */
-var ageReaction = age + " year" + (age > 1 ? "s" : "") + " old?\nThat's ";
-if (age < 20) {
-	ageReaction += "pretty young!";
-} else if (age < 50) {
-	ageReaction += "pretty old!";
-} else if (age < 80) {
-	ageReaction += "unhuman...";
+	/* Test result and react accordingly */
+	if (!isNaN(name)) {
+		if (timesAsked++ <= 2) {
+			alert("Ok that's not really your name...");
+		} else {
+			name = defaultName;
+			alert("Ok let's just name you " + name + "...");
+			break;
+		}
+	} else {
+		alert(name + "?\nWow that's a nice name!");
+		break;
+	}
 }
 
-alert(ageReaction);
+var age = "string";
+timesAsked = 1;
+while (true) {
+	age = prompt("And how old are you " + name + "?"); 
+
+	/* Test result and react accordingly */
+	if (isNaN(age)) {
+		if (timesAsked++ <= 2) {
+			alert("What?" + ((timesAsked === 3) ? "!" : ""));
+		} else {
+			alert("I'm done with you, hopeless human.");
+			break;
+		}
+	} else {
+		break;
+	}
+}
+
+if (!isNaN(age)) {
+	/* Reveal the real age */
+	age *= 1.6;
+	/* Truncate */
+	age -= age % 1
+
+		/* Build reaction string */
+		var ageReaction = age + " year" + (age > 1 ? "s" : "") + " old?\nThat's ";
+		if (age < 20) {
+			ageReaction += "pretty young!";
+		} else if (age < 50) {
+			ageReaction += "pretty old!";
+		} else if (age < 80) {
+			ageReaction += "unhuman...";
+		}
+
+		alert(ageReaction);
+}
