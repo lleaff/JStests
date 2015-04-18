e7c1788c5d981bb49400ab8d326d5043bc7acba9
lleaff <lleaff@users.noreply.github.com>
2015-04-18 15:22:07 +0200

    working with string too

--- eloquent/functionPropertiesTest.js
+++ eloquent/functionPropertiesTest.js
@@ -13,3 +13,20 @@ function myDummyFunc(dumArg)
 for (var i = 0; i < 5; ++i) {
 	console.log(myDummyFunc());
 }
+
+
+function myDummyStrFunc(dumArg)
+{
+	if (myDummyStrFunc.dumStrProp === undefined) {
+		console.log("INITIALIZING dumStrProp");
+		myDummyStrFunc.dumStrProp = "a";
+	}
+
+	myDummyStrFunc.dumStrProp += "a";
+
+	return myDummyStrFunc.dumStrProp;
+}
+
+for (var i = 0; i < 5; ++i) {
+	console.log(myDummyStrFunc());
+}
