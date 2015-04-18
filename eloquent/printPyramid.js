ab97d7f963b462bd1e4fb34c2a794faac7136c32
lleaff <lleaff@users.noreply.github.com>
2015-04-18 15:48:09 +0200

    progress

--- eloquent/printPyramid.js
+++ eloquent/printPyramid.js
@@ -4,15 +4,16 @@ function drawPyramid(width, brick = "@", space = " ")
 {
 	var height = width / 2;
 	for (var y = 1; y <= height; ++y) {
-		for (var x = 0; x < (width - y) / 2; ++x) {
+		for (var x = 0; x < (width / 2) - y; ++x) {
 			print(space);
 		}
-		for (; x < width; ++x) {
+		for (; x < (width / 2) + y - 0.5; ++x) {
 			print(brick);
 		}
 		for (; x < width; ++x) {
 			print(space);
 		}
+		console.log("DEBUG:\\n");
 		print("\n");
 	}
 }
@@ -27,8 +28,8 @@ function print(str)
 	if (typeof(str) == "string") {
 		print.strbuffer += str;
 		if (print.strbuffer[print.strbuffer.length - 1] === "\n") {
-			print.strbuffer[print.strbuffer.length - 1] = '';
-			console.log(print.strbuffer);
+			print.strbuffer[print.strbuffer.length - 1] = "";
+			console.log(print.strbuffer + "<<<");
 		}
 	} else {
 		console.log("ERROR: print: \""+ str + "\" is not a string");
