70fde74db6d23a2c472b6888642136f371a6b284
lleaff <lleaff@users.noreply.github.com>
2015-04-19 02:39:45 +0200

    compact fizzbuzz

--- eloquent/fizzBuzz.js
+++ eloquent/fizzBuzz.js
@@ -1,2 +1,8 @@
 var range = [1, 100];
 
+for (var i = range[0]; i <= range[1]; ++i) {
+	if     (i % 15 === 0)	console.log("FizzBuzz");
+	else if(i % 3 === 0)	console.log("Fizz");
+	else if(i % 5 === 0)	console.log("Buzz");
+	else 					console.log(i);
+}
