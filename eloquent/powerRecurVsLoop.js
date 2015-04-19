886aa6830ab681cda5afb404b0de581c2a55dae2
lleaff <lleaff@users.noreply.github.com>
2015-04-20 00:56:42 +0200

    +print time elapsed with console.time() & console.timeEnd()

--- eloquent/powerRecurVsLoop.js
+++ eloquent/powerRecurVsLoop.js
@@ -19,7 +19,11 @@ function powLoop(base, exponent)
 console.log("2^10 =");
 
 console.log("Recur:");
+console.time(powRecur); //PERF
 console.log(powRecur(2, 10));
+console.timeEnd(powRecur); //PERF
 
 console.log("Loop:");
+console.time(powRecur); //PERF
 console.log(powRecur(2, 10));
+console.timeEnd(powRecur); //PERF
