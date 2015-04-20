d2ad5a3d955995e44d3f93d0fb6d83d880447abb
lleaff <lleaff@users.noreply.github.com>
2015-04-20 17:52:06 +0200

    works

--- eloquent/addMultToReachTarget.js
+++ eloquent/addMultToReachTarget.js
@@ -3,6 +3,45 @@ console.log("For each number, print the shortest sequence of operations required
 
 var targets = [ 13, 15, 24, 511 ];
 
-targets.for
+for (var i = 0; i < targets.length; ++i) {
+	console.log(targets[i] + ':\n' + (solveProblem(targets[i]) || "Can't reach"));
+}
 
-function solveProblem(target);
+function solveProblem(target) {
+	var start = 1;
+	var successfullOperationRoutes = ["none"];
+	var currNum = 1;
+
+	function find(currNum, prevOperations) {
+		if (currNum === target) {
+			successfullOperationRoutes.push(prevOperations);
+			return null;
+		} else if (currNum > target) {
+			return null;
+		} else {
+			return find(currNum + 5, prevOperations + "+") ||
+				find(currNum * 3, prevOperations + "*");
+		}
+	}
+	find(start, "");
+
+	if (successfullOperationRoutes.length === 1) {
+		return null;
+	} else {
+
+		// Need to find the maximum value to have something to compare the values to after
+		var max = 0;
+		for (var i = 1; i < successfullOperationRoutes.length; ++i) {
+			if (successfullOperationRoutes[i].length > max) max = successfullOperationRoutes[i].length;
+		}
+		var min = max;
+		var minIndex;
+		for (i = 1; i < successfullOperationRoutes.length; ++i) {
+			if (successfullOperationRoutes[i].length <= min) {
+				min = successfullOperationRoutes[i];
+				minIndex = i;
+			}
+		}
+		return successfullOperationRoutes[minIndex];
+	}
+}
