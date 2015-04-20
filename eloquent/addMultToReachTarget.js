0a3e2989105444fd0c6c52c020dcc18a85a23a82
lleaff <lleaff@users.noreply.github.com>
2015-04-20 16:45:05 +0200

    problem exposed

---
+++ eloquent/addMultToReachTarget.js
@@ -0,0 +1,8 @@
+console.log("You have two operations available:\n\tAdd 5\n\t Multiply by 3");
+console.log("For each number, print the shortest sequence of operations required to go from 1 to the target");
+
+var targets = [ 13, 15, 24, 511 ];
+
+targets.for
+
+function solveProblem(target);
