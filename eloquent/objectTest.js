4b90e42a19372ac6e80f35456a046f55b3ea254c
lleaff <lleaff@users.noreply.github.com>
2015-04-21 23:52:29 +0200

    +this usage

--- eloquent/objectTest.js
+++ eloquent/objectTest.js
@@ -1,8 +1,13 @@
 var animal = {
+	name: "Richard",
 	size: 5,
 	talk: function yap(str) {
 		console.log(str.toUpperCase() + "!!!");
+	},
+	introduce: function() {
+		console.log(this.talk("My name is " + this.name));
 	}
 };
 
 animal.talk("hello you");
+animal.introduce();
