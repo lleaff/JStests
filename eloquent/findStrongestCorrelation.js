07e0b340103120438e990e62a47661d569f7d4d4
lleaff <lleaff@users.noreply.github.com>
2015-04-23 15:30:01 +0200

    +hasEvent()

--- eloquent/findStrongestCorrelation.js
+++ eloquent/findStrongestCorrelation.js
@@ -2,3 +2,10 @@
 if (JOURNAL === undefined) {
 	throw new Error("JOURNAL not found, no file to operate on");
 }
+
+// string: journalEvent
+function hasEvent(journalEvent)
+{
+	// indexOf returns -1 if it doesn't find the element
+	return JOURNAL.entry.indexOf(journalEvent) !== -1;
+}
