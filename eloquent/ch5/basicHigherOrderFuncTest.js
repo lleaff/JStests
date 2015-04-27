97f05b3a4fb301c74431f2ab8e73df9ab9856d1d
lleaff <lleaff@users.noreply.github.com>
2015-04-27 17:31:31 +0200

    +advForEach(), allows passing functions taking multiple arguments directly without closure

--- eloquent/ch5/basicHigherOrderFuncTest.js
+++ eloquent/ch5/basicHigherOrderFuncTest.js
@@ -1,3 +1,5 @@
+var testArr = [ 9, 8, 7, 6, 5, 4 ];
+
 Object.prototype.basicForEach =  function(fn) {
 	if (!this.hasOwnProperty('0')) throw new Error(this.toString + "is not an array");
 
@@ -6,6 +8,29 @@ Object.prototype.basicForEach =  function(fn) {
 	}
 };
 
-var testArr = [ 9, 8, 7, 6, 5, 4 ];
-
 testArr.basicForEach(function(a) { console.log(a + 1); });
+
+
+Object.prototype.advForEach = function(fn, arrValPos) {
+	if (!this.hasOwnProperty('0')) throw new Error(this.toString + "is not an array");
+
+	var callingObj = this;
+	var args = arguments;
+	var argsLength = arguments.length;
+
+	var cfn = function(elem) {
+		// Build the array of arguments
+		var callArgs = [];
+		for (var i = 0, offset = 0; i < argsLength - 2 - offset; ++i) {
+			if ( i == arrValPos ) { callArgs[i] = elem; --offset; }
+			else callArgs[i] = args[i + 2 + offset];
+		}
+		fn.apply(callingObj, callArgs);
+	};
+
+	this.basicForEach(cfn);
+};
+
+function print() { console.log.apply(console, arguments); }
+
+testArr.advForEach(print, 0, "2nd arg", "3rd arg");
