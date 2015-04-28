1ba81ea8c244685be74f27ecfcac4a17bfc021d4
lleaff <lleaff@users.noreply.github.com>
2015-04-28 18:37:10 +0200

    JSON.stringify()

--- eloquent/ch5/filterAncestry.js
+++ eloquent/ch5/filterAncestry.js
@@ -1,6 +1,10 @@
 if (ANCESTRY_FILE === undefined)
-	throw new Error("ANCESTRY_FILE undefined, ancestry.js file not included?");
+	throw new Error("ANCESTRY_FILE undefined, include ancestry");
 
 var ancestry = JSON.parse(ANCESTRY_FILE);
 
-console.log(ancestry.length);
+console.log("length: "+ancestry.length);
+
+console.log("JSON.stringify(): \n"+
+			JSON.stringify(ancestry[3], ["name", "sex", "born", "died"], 1));
+console.log("ancestry[3].name: "+ancestry[3].name);
