ac1ffc0f65cfb0dda959a87083c91e8f43b695d9
lleaff <lleaff@users.noreply.github.com>
2015-04-29 22:11:09 +0200

    >getDeathAge to use either index or direct object argument

--- eloquent/ch5/filterAncestry.js
+++ eloquent/ch5/filterAncestry.js
@@ -2,14 +2,35 @@ if (ANCESTRY_FILE === undefined)
 	throw new Error("ANCESTRY_FILE undefined, include ancestry");
 
 var ancestry = JSON.parse(ANCESTRY_FILE);
-ancestry.getAge = function(index)
+ancestry.getDeathAge = function(indexOrObject)
 {
-	return (this[index].died - this[index].born);
+	if (indexOrObject === undefined) 
+		throw new Error("indexOrObject: no argument");
+
+	var obj;
+	if		(typeof(indexOrObject) === "object") obj = indexOrObject;
+	else if	(typeof(indexOrObject) === "number") obj = this[indexOrObject];
+	return (obj.died - obj.born);
 };
 
 console.log("length: "+ancestry.length);
 
 console.log("JSON.stringify(): \n" +
 			JSON.stringify(ancestry[3], ["name", "sex", "born", "died"], 1) +
-			"\"age\":" + ancestry.getAge(3));
+			"\"age\":" + ancestry.getDeathAge(3));
 console.log("ancestry[3].name: " + ancestry[3].name);
+
+function arrayFilter(array, test)
+{
+	var valid = [];
+	for (var i = 0; i < array.length; ++i)
+		if (test(array[i]))
+			valid.push(array[i]);
+	return valid;
+}
+
+var deathAgeLimit = 80;
+console.log("\nPeople who lived longer than " + deathAgeLimit + " years old:\n");
+arrayFilter(ancestry, 
+			function(a) { return ancestry.getDeathAge(a) > deathAgeLimit; } 
+		   ).forEach(function(a) { console.log(a.name); });
