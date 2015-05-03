59c7a1184100c279e8cf9ef1f1f5f6129107932a
lleaff <lleaff@users.noreply.github.com>
2015-05-03 02:26:55 +0200

    set style using HTMLElement.style and Node.setAttribute

--- eloquent/ch5/basicDOMManip.js
+++ eloquent/ch5/basicDOMManip.js
@@ -3,6 +3,20 @@ button.appendChild(document.createTextNode("Click me!"));
 button.onclick = function() {
 	alert("Yo");
 };
+button.style = { backgroundColor: "#f00" };
+
+var fakeButt = document.createElement("div");
+// <div></div>
+fakeButt.appendChild(document.createTextNode("button-ton!"));
+// <div>button-ton</div>
+fakeButt.setAttribute("style",
+					  "background-color: #100; color: #ff6; display: block; font-size: 1em; width: 6em");
+//<div style="backgr...">button-ton</div>
+fakeButt.onclick = function() {
+alert("I'm a button!");
+};
+
 window.onload = function() {
 	document.body.appendChild(button);
+	document.body.insertBefore(fakeButt, button);
 };
