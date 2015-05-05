c04b1853a920e29afee1aa087c93804d24eec5c4
lleaff <lleaff@users.noreply.github.com>
2015-05-05 23:41:20 +0200

    +htmlButton() initializer for button nodes, TODO: find a better way than using eval for passing in not yet existing elements

--- eloquent/ch5/basicSetTimeout.js
+++ eloquent/ch5/basicSetTimeout.js
@@ -1,5 +1,29 @@
 var onWindowLoad = []; /* Array of functions to execute when window is loaded */
 
+/* "button" node initialization
+ *  eg: htmlButton("click me", alert("thx"), "document.body");
+ *      htmlButton("me first", alert("ty!"), "document.body", document.body[0]);
+ *   => <body>
+ *          <button onclick=alert("ty!")>me first</button>
+ *          <button onclick=alert("thx")>click me</button>
+ *      </body>                                                             */
+function htmlButton(text, onclick, refElem, beforeThisElement)
+{
+	var but = document.createElement("button");
+	if (text !== undefined && text !== null) but.appendChild(document.createTextNode(text));
+	if (onclick) but.onclick = onclick;
+	if (refElem !== undefined) {
+		if (beforeThisElement === undefined)
+			onWindowLoad.push(function() { 
+				(typeof refElem === "string" ? (eval(refElem)) : refElem).appendChild(but); });
+		else
+			onWindowLoad.push(function() { 
+				(typeof refElem === "string" ? (eval(refElem)) : refElem).insertBefore(but, beforeThisElement);
+			});
+	}
+	return but;
+}
+
 (function() {
 	var but = document.createElement("button");
 	but.appendChild(document.createTextNode("button"));
@@ -7,6 +31,8 @@ var onWindowLoad = []; /* Array of functions to execute when window is loaded */
 	onWindowLoad.push(function() { document.body.insertBefore(but, document.body[0]); });
 })();
 
+var but2 = htmlButton("Button n°2", console.log.bind(console, "n°2 clicked"), "document.body");
+var but1 = htmlButton("Button n°1", console.log.bind(console, "n°1 clicked"), "document.body", but2);
 
 
 window.onload = function() { onWindowLoad.forEach(function(el) { el(); }); };
