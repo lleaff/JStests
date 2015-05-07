f128d7b5d3939e62e98d34d0fde8db680cef2b33
lleaff <lleaff@users.noreply.github.com>
2015-05-08 00:50:04 +0200

    +override workOnAncestry()

---
+++ eloquent/ch5/lifeExpectancy.js
@@ -0,0 +1,15 @@
+if (Array.prototype.average === undefined)
+	throw new Error("No array.average(), include average.js before this.");
+
+/* Find the <script src="ancestry.js"> node created in average.js */
+var ancestryScript = Array.filter(
+	document.getElementsByTagName("script"), 
+	function(a){ return a.getAttribute("src") === "ancestry.js"; })[0];
+
+/* Override it's onload function */
+ancestryScript.onload = workOnAncestry;
+
+function workOnAncestry() 
+{
+	console.log("hi");
+}
