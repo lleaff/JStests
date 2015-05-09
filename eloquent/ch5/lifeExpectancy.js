956e96ee70a9f4bc3bde75c0af59bf1f4555f474
lleaff <lleaff@users.noreply.github.com>
2015-05-09 15:48:36 +0200

    +lifeExpectancy(set)

--- eloquent/ch5/lifeExpectancy.js
+++ eloquent/ch5/lifeExpectancy.js
@@ -6,12 +6,23 @@ var ancestryScript = Array.filter(
 	document.getElementsByTagName("script"), 
 	function(a){ return a.getAttribute("src") === "ancestry.js"; })[0];
 
-/* Override it's onload function */
+/* Override its onload function */
 ancestryScript.onload = workOnAncestry;
 
 function workOnAncestry() 
 {
-	console.log("hi");
+	var ancestry = JSON.parse(ANCESTRY_FILE);
+
+	function getAge(person) { return person.died - person.born; }
+
+	function lifeExpectancy(arr) {
+		return arr.map(getAge).filter(
+			function(a){ return (typeof a === "number" && !isNaN(a)); }
+		).average(); 
+	}
+
+	console.log("Overall life expectancy:\n\t" + 
+				(lifeExpectancy(ancestry)>>0) );
 }
 
 console.log("---lifeExpectancy.js:\tSynchronous execution done---");
