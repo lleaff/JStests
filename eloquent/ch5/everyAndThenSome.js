6b58e589109fab00dcdf181a2f5127511084e94c
lleaff <lleaff@users.noreply.github.com>
2015-05-10 15:43:49 +0200

    >provide index and array values to predicate functions

--- eloquent/ch5/everyAndThenSome.js
+++ eloquent/ch5/everyAndThenSome.js
@@ -1,14 +1,16 @@
 Array.prototype.customSome = function(fn)
 {
 	var result = false;
-	this.forEach(function(a){ if(fn(a)) result = true; });
+	this.forEach(
+		function(val, i, arr){ if(fn(val, i, arr)) result = true; });
 	return result;
 };
 
 Array.prototype.customEvery = function(fn)
 {
 	var result = true;
-	this.forEach(function(a) { if(!fn(a)) result = false; });
+	this.forEach(
+		function(val, i, arr) { if(!fn(val, i, arr)) result = false; });
 	return result;
 };
 
@@ -23,9 +25,13 @@ Array.prototype.customEvery = function(fn)
 			every: false, some: true },
 		{ arr: [ "1", "2", "3", "4", "5" ],
 			test: function(a){ return typeof a === "number"; },
-			every: false, some: false } ];
+			every: false, some: false },
+		{ arr: [1, 2, "3", "4", 5 ],
+			test: function(val, i, arr){ 
+				return (i === 0) ? true : arr[i - 1] == (val>>0) - 1; },
+			every: true, some: true } ];
 
-		tests.forEach(function(obj, i){
+		tests.forEach(function(obj, i, arr){
 			console.log("test n°"+(i+1));
 			console.log("some:\t" + 
 				(obj.arr.customSome(obj.test) === obj.some ?
