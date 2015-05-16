2662c037ac7a0188446f70ca15626d9218c5ac25
lleaff <lleaff@users.noreply.github.com>
2015-05-16 23:48:50 +0200

    +base for sequence interface

---
+++ eloquent/ch6/sequence.js
@@ -0,0 +1,12 @@
+Object.defineProperty(Array.prototype, "sequenceInterface", {
+	configurable: false, enumerable: false, 
+	value: {  }
+});
+
+Array.prototype.next = function() {
+
+};
+
+Array.prototype.previous = function() {
+
+};
