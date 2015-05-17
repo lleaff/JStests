08e4e12f508975da0d793f5ee99794b0570ae7fd
lleaff <lleaff@users.noreply.github.com>
2015-05-17 16:38:23 +0200

    +test with longer array and move pointer before test

--- eloquent/ch6/sequenceTests.js
+++ eloquent/ch6/sequenceTests.js
@@ -1,5 +1,7 @@
-var testArr1 = [ "one", "two", "three", "four", "five" ];
+var testArr1 = [ "one", "two", "three", "four", "five", "six", "seven" ];
 var testArr2 = [ "one", "two" ];
+
+testArr1.next();
 
 console.log("testArr1:"); logFive(testArr1);
 console.log("testArr2:"); logFive(testArr2);
