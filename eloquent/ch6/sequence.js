0c2aaeb4e7724348a5d059f3833c6b6ea4182b6b
lleaff <lleaff@users.noreply.github.com>
2015-05-17 02:08:18 +0200

    trying to encapsulate internal interface properties in a dedicated object property, caveat being the need to initialize the reference to parent element in the interface object

--- eloquent/ch6/sequence.js
+++ eloquent/ch6/sequence.js
@@ -1,6 +1,23 @@
-Object.defineProperty(Array.prototype, "sequenceInterface", {
-	configurable: false, enumerable: false, 
-	value: {  }
+function buildSequenceInterfaceOn(prototypeObject) {
+	Object.defineProperty(prototypeObject, "sequenceInterface", {
+		configurable: false, enumerable: false, 
+		value: {  
+			currentIndex: 0,
+			self: null
+		}
+	});
+
+	Object.defineProperty(prototypeObject, "initSequenceInterface", {
+		configurable: false, enumerable: false, 
+		value: function() { 
+			console.log("init: ", this);
+			this.sequenceInterface.self = this; return this; }
+	});
+}
+buildSequenceInterfaceOn(Array.prototype);
+
+Object.defineProperty(Array.prototype.sequenceInterface, "length", {
+	get: function() { return this.self && this.self.length; }
 });
 
 Array.prototype.next = function() {
@@ -10,3 +27,5 @@ Array.prototype.next = function() {
 Array.prototype.previous = function() {
 
 };
+
+var testArr = [ "one", "two", "three", "four", "five" ];
