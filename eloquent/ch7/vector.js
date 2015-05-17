9cbf78b7a9b2e41b1f9999878ce5053f3af601a6
lleaff <lleaff@users.noreply.github.com>
2015-05-17 22:28:07 +0200

    ~add -> plus, substract -> minus & +add, substract: modify vector directly

---
+++ eloquent/ch7/vector.js
@@ -0,0 +1,11 @@
+function Vector(x, y) { this.x = +x; this.y = +y; }
+
+Vector.prototype.plus = function(vec) {
+	return new Vector(this.x + vec.x, this.y + vec.y); };
+
+Vector.prototype.minus = function(vec) {
+	return new Vector(this.x - vec.x, this.y - vec.y); };
+
+Vector.prototype.add = function(x, y) { this.x += x; this.y += y; };
+
+Vector.prototype.substract = function(x, y) { this.x -= x; this.y -= y; };
