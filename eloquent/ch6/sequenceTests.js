d8a9a6d85e262f8c53834ea71943fe1e9d1c72d6
lleaff <lleaff@users.noreply.github.com>
2015-05-17 16:56:11 +0200

    +test sequences of numbers and characters

--- eloquent/ch6/sequenceTests.js
+++ eloquent/ch6/sequenceTests.js
@@ -5,3 +5,15 @@ testArr1.next();
 
 console.log("testArr1:"); logFive(testArr1);
 console.log("testArr2:"); logFive(testArr2);
+
+console.log(Array(61).join("-"));
+
+var TwoToFiftyRange = new RangeSeq(2, 50);
+var OneToFourRange = new RangeSeq(1, 4);
+
+console.log("[2-50]:"); logFive(TwoToFiftyRange);
+console.log("[1-4]:"); logFive(OneToFourRange);
+
+var aToZ = new RangeSeq("a", "z");
+
+console.log("[a-z]:"); logFive(aToZ);
