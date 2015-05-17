4f5e1dd87ebe5c121d6066b357d44ba2c1f45133
lleaff <lleaff@users.noreply.github.com>
2015-05-17 15:36:37 +0200

    +pass

---
+++ eloquent/ch6/sequenceTests.js
@@ -0,0 +1,5 @@
+var testArr1 = [ "one", "two", "three", "four", "five" ];
+var testArr2 = [ "one", "two" ];
+
+console.log("testArr1:"); logFive(testArr1);
+console.log("testArr2:"); logFive(testArr2);
