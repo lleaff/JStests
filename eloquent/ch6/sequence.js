5b54a6c61728b6b215c8e82e4eab9612e9048ba2
lleaff <lleaff@users.noreply.github.com>
2015-05-17 15:09:12 +0200

    ~stop trying to modify native contrustors (bad bad) & +working interface: .next(), previous(), and .current

--- eloquent/ch6/sequence.js
+++ eloquent/ch6/sequence.js
@@ -1,31 +1,28 @@
-function buildSequenceInterfaceOn(prototypeObject) {
-	Object.defineProperty(prototypeObject, "sequenceInterface", {
-		configurable: false, enumerable: false, 
-		value: {  
-			currentIndex: 0,
-			self: null
-		}
-	});
+Object.defineProperty(Array.prototype, "_length", {
+	get: function() { return this.length; }
+});
 
-	Object.defineProperty(prototypeObject, "initSequenceInterface", {
-		configurable: false, enumerable: false, 
-		value: function() { 
-			console.log("init: ", this);
-			this.sequenceInterface.self = this; return this; }
-	});
-}
-buildSequenceInterfaceOn(Array.prototype);
+Object.defineProperty(Array.prototype, "currentIndex", {
+	get: function() { 
+		if (this._currentIndex === undefined) this._currentIndex = 0;
+		return this._currentIndex; },
+	set: function(a) { 
+		if (a >= 0 && a < this._length) this._currentIndex = a; }
+});
 
-Object.defineProperty(Array.prototype.sequenceInterface, "length", {
-	get: function() { return this.self && this.self.length; }
+Object.defineProperty(Array.prototype, "current", {
+	get: function() { return this[this.currentIndex]; },
+	set: function(a) { this[this.currentIndex] = a; }
 });
 
 Array.prototype.next = function() {
-
+	var prevIndex = this._currentIndex;
+	this.currentIndex = prevIndex + 1;
+	return prevIndex !== this._currentIndex;
 };
 
 Array.prototype.previous = function() {
-
+	var prevIndex = this._currentIndex;
+	this.currentIndex = prevIndex - 1;
+	return prevIndex !== this._currentIndex;
 };
-
-var testArr = [ "one", "two", "three", "four", "five" ];
