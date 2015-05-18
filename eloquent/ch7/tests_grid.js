b8fa8f51f36cea795709c2edb8b54c9e27404267
lleaff <lleaff@users.noreply.github.com>
2015-05-18 22:46:06 +0200

    isOutside working

--- eloquent/ch7/tests_grid.js
+++ eloquent/ch7/tests_grid.js
@@ -6,4 +6,7 @@ var mark = new Vector(3, 3);
 
 testGrid.set(mark, "X");
 console.log(testGrid.draw());
-console.log(testGrid.get(mark));
+console.log(mark+": "+testGrid.get(mark));
+
+console.log("is "+mark+" inside the grid? "+
+			(!testGrid.isOutside(mark) ? "yes" : "no"));
