aa3a22a6c6433d5d96b59747b68277d4df631022
lleaff <lleaff@users.noreply.github.com>
2015-05-18 22:39:08 +0200

    set and get working

--- eloquent/ch7/tests_grid.js
+++ eloquent/ch7/tests_grid.js
@@ -1,3 +1,9 @@
 var testGrid = new Grid(10, 5);
 testGrid.fill("@");
 console.log(testGrid.draw());
+
+var mark = new Vector(3, 3);
+
+testGrid.set(mark, "X");
+console.log(testGrid.draw());
+console.log(testGrid.get(mark));
