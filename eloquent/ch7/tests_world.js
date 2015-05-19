a76df66d1758cf97b1c035355b491a98bf1d7f6f
lleaff <lleaff@users.noreply.github.com>
2015-05-19 21:32:09 +0200

    ~separate tests

---
+++ eloquent/ch7/tests_world.js
@@ -0,0 +1,11 @@
+var testPlan = [
+	"############",
+	"#          #",
+	"#  ##   o  #",
+	"#    #     #",
+	"#  o       #",
+	"#        o #",
+	"############"].join("\n");
+var testWorld1 = new World(testPlan);
+
+console.log(""+testWorld1.grid);
--- eloquent/ch7/world.js
+++ eloquent/ch7/world.js
@@ -3,15 +3,3 @@ function World(plan) {
 	this.grid = grid;
 
 }
-
-var testPlan = [
-	"############",
-	"#          #",
-	"#  ##   o  #",
-	"#    #     #",
-	"#  o       #",
-	"#        o #",
-	"############"].join("\n");
-var testWorld1 = new World(testPlan);
-
-console.log(""+testWorld1.grid);
