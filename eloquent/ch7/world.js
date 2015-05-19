94a946b67da27a3aa67dd43378f21f2a8771572d
lleaff <lleaff@users.noreply.github.com>
2015-05-19 23:39:27 +0200

    +world.toString(), wrap around grid.toString()

--- eloquent/ch7/world.js
+++ eloquent/ch7/world.js
@@ -1,5 +1,8 @@
 function World(plan) {
 	var grid = (typeof plan === "object") ? plan : Grid.parse(plan);
 	this.grid = grid;
-
 }
+
+World.prototype.toString = function() {
+	return this.grid.toString();
+};
