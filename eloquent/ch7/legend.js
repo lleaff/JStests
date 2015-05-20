f9bd05bf85183127f199428feeb4bbe631eecd03
lleaff <lleaff@users.noreply.github.com>
2015-05-21 01:16:18 +0200

    ~add a .ch property to the element objects & +build the character>constructor map automatically

--- eloquent/ch7/legend.js
+++ eloquent/ch7/legend.js
@@ -1,28 +1,53 @@
 var legend = (function() {
 
-	/* =Character map
-	 * ------------------------------------------------------------ */
-	var legend = {
-		"#": [ Wall ],
-		" ": [ EmptySpace ],
-		"o": [ Critter, EmptySpace]
-	};
-
 	/* =Constructors
 	 * ------------------------------------------------------------ */
-	function EmptySpace() {
-		this.type = "emptySpace";
-		this.solid = false;
-	}
+	var constructors = {
+		EmptySpace: function() {
+			this.type 	= "emptySpace";
+			this.ch		= " ";
+			this.solid	= false;
+		},
 
-	function Wall() {
-		this.type = "wall";
-		this.solid = true;
-	}
+		Wall: function() {
+			this.type 	= "wall";
+			this.ch		= "#";
+			this.solid 	= true;
+		},
 
-	function Critter() {
-		this.type = "critter";
-		this.solid = true;
+		Critter: function() {
+			this.type 	= "critter";
+			this.ch		= "o";
+			this.solid 	= true;
+			this.speed 	= 1;
+		},
+
+		BouncingCriter: function() {
+			this.type	= "bouncingCriter";
+			this.ch		= "@";
+			this.solid 	= true;
+			this.speed 	= 1;
+			this.act	= bouncingCriterAct;
+		},
+	};
+
+	/* =Character map
+	 * ------------------------------------------------------------ */
+	/* Build a map of characters->constructors */
+	var legend = (function() {
+		var legend = {};
+		for (var constructor in constructors) {
+			/* Create a new element from the constructor to be able to
+			 *  extract its ch property */
+			var elem = new constructors[constructor]();
+			legend[elem.ch] = [ constructors[constructor] ];
+			/* If the element can move, put empty space under it */
+			if (elem.speed) legend[elem.ch].push(constructors.EmptySpace);
+		}
+		return legend;
+	})();
+
+
 	}
 
 	return legend;
