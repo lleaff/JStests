10092825eec79bbf85553f3b1157a2bab9a74ecf
lleaff <lleaff@users.noreply.github.com>
2015-05-20 17:23:46 +0200

    ~move legend in separate file, put constructors in separate functions, and wrap it all in a self executing function

---
+++ eloquent/ch7/legend.js
@@ -0,0 +1,29 @@
+var legend = (function() {
+
+	/* =Character map
+	 * ------------------------------------------------------------ */
+	var legend = {
+		"#":  Wall,
+		" ": EmptySpace,
+		"o": Critter
+	};
+
+	/* =Constructors
+	 * ------------------------------------------------------------ */
+	function EmptySpace() {
+		this.type = "emptySpace";
+		this.solid = false;
+	}
+
+	function Wall() {
+		this.type = "wall";
+		this.solid = true;
+	}
+
+	function Critter() {
+			this.type = "critter";
+			this.solid = true;
+	}
+
+	return legend;
+})();
