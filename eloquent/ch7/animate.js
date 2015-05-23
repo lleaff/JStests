ba93b09a4757174da3ba5869f2d7b5ae661b2cf2
lleaff <lleaff@users.noreply.github.com>
2015-05-23 16:55:22 +0200

    ~build a LivingWorld object

--- eloquent/ch7/animate.js
+++ eloquent/ch7/animate.js
@@ -1,13 +1,19 @@
-function LivingWorld() {
+function LivingWorld(htmlNode, plan) {
 	/* Drawing surface */
-	var surfaceContainer = document.getElementById("world");
-	if (!surfaceContainer)
-		throw new Error(
-			"no #world found, create a container for world surface");
-	var surface = surfaceContainer.children[0];
-	if (!surface || surface.tagName !== "PRE") {
-		surface = document.createElement("PRE");
-		surfaceContainer.appendChild(surface);
-	} 
-	this.surface = surface;
+	this.surface = (function() {
+		if (!htmlNode) htmlNode = document.getElementById("world");
+		if (!htmlNode)
+			throw new Error(
+				"no #world found, create a container for world surface");
+
+		var surface = htmlNode.children[0];
+		if (!surface || surface.tagName !== "PRE") {
+			surface = document.createElement("PRE");
+			htmlNode.appendChild(surface);
+		} 
+		return surface;
+	})();
+
+	/* World */
+	this.world = new World(plan);
 }
