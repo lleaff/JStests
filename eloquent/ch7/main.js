e0e61d07e3491eb9ea1fcfb1793eee0be59db1ec
lleaff <lleaff@users.noreply.github.com>
2015-05-23 19:55:16 +0200

    +data and basic view

---
+++ eloquent/ch7/main.js
@@ -0,0 +1,29 @@
+//(function() {
+	var updateTime = 500;
+
+	var worlds = Data.plans.map(function(plan) {
+		var container = document.createElement("DIV");
+		container.setAttribute("class", "world");
+		document.body.appendChild(container);
+
+		return new LivingWorld(plan, container);
+	});
+
+	function startAnimation(world) {
+		world.intervalId = setInterval(world.tick.bind(world), updateTime);
+	}
+	function stopAnimation(world) {
+		clearInterval(world.intervalId);
+		world.intervalId = null;
+	}
+	function toggleAnimation(world) {
+		if (!world.intervalId) startAnimation(world);
+		else stopAnimation(world);
+	}
+
+	worlds.forEach(function(world) {
+		world.surface.onclick = toggleAnimation(world);
+		startAnimation(world);
+	});
+
+//})();
---
+++ eloquent/ch7/plans.js
@@ -0,0 +1,13 @@
+if (Data === undefined) var Data = {};
+
+Data.plans = (function() { return [
+		[
+			"############",
+			"#          #",
+			"#  ##   o  #",
+			"#    #     #",
+			"#  @       #",
+			"#        o #",
+			"############"
+		],
+	].map(function(plan) {return plan.join("\n"); }); })();
