3a6cb0fb349c04885b1ffdefe871930157d50243
lleaff <lleaff@users.noreply.github.com>
2015-05-24 15:36:54 +0200

    >shuffleArray(), wrong conditional check

--- eloquent/ch7/miscHelperFunctions.js
+++ eloquent/ch7/miscHelperFunctions.js
@@ -2,7 +2,7 @@
  * ------------------------------------------------------------ */
 function shuffleArray(arr) {
 	for (var i = 0, len = arr.length, visited = []; i < len; ++i) {
-		if (visited.indexOf(i) === -1) continue;
+		if (visited.indexOf(i) !== -1) continue;
 		var rand = Math.random()*len>>0;
 		visited.push(rand);
 		
