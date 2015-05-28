554d1cbcd10589f2fd55cc99e116426385ba0e60
lleaff <lleaff@users.noreply.github.com>
2015-05-28 15:53:42 +0200

    ~put filenames in var

--- eloquent/ch7/gulpfile.js
+++ eloquent/ch7/gulpfile.js
@@ -19,27 +19,30 @@ gulp.task('build', ['js', 'css', 'html']);
 
 gulp.task('js', function() {
 	var jsfolder = srcD+'/js/';
+	var jsFilenames = [
+		'miscHelperFunctions.js',
+		'vector.js',
+		'grid.js',
+		'world.js',
+		'world_directions.js',
+		'world_perception.js',
+		'world_logic.js',
+		'ai.js',
+		'color.js',
+		'legend.js',
+		'plans.js',
+		'animate.js',
+		'main.js'
+	];
+
 	return merge (
 		gulp.src([jsfolder+'*.js',
 				 '!'+jsfolder+'main.js',
 				 '!'+jsfolder+'test*']),
 		gulp.src(jsfolder+'main.js')
 			.pipe(insert.append('main()')))
-			.pipe(order([
-				'miscHelperFunctions.js',
-				'vector.js',
-				'grid.js',
-				'world.js',
-				'world_directions.js',
-				'world_perception.js',
-				'world_logic.js',
-				'ai.js',
-				'color.js',
-				'legend.js',
-				'plans.js',
-				'animate.js',
-				'main.js'
-			].map(function(filename) { return '**/'+filename; })))
+			.pipe(order(jsFilenames.map(
+				function(filename) { return '**/'+filename; })))
 			.pipe(sourcemaps.init())
 				.pipe(concat('all.js'))
 				.pipe(uglify())
