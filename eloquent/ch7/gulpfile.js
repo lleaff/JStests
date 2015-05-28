b4c1c7ae71971424315bb55d1c4942231aed944f
lleaff <lleaff@users.noreply.github.com>
2015-05-28 21:15:34 +0200

    +license task, prepend comments with license termson output files

--- eloquent/ch7/gulpfile.js
+++ eloquent/ch7/gulpfile.js
@@ -19,6 +19,11 @@ var paths = {
 	build: './build',
 };
 
+package.pretty = {
+	name: package.name.replace('-',' '),
+	author: package.author,
+};
+
 var mainJs = 'all.js';
 
 /* =Tasks
@@ -73,4 +78,34 @@ gulp.task('html', function() {
 	return gulp.src(paths.source+'/*html')
 		.pipe(gulp.dest(paths.build));
 });
+
+gulp.task('license', function() {
+	var copyrightText =
+		"Copyright "+new Date().getFullYear()+" "+
+		package.pretty.author+"\n";
+	var licenseText =
+		"This file is part of "+package.pretty.name+".\n\n"+package.pretty.name+" is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\n"+package.pretty.name+" is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with "+package.pretty.name+".  If not, see <http://www.gnu.org/licenses/>.\n";
+
+	var headerText = copyrightText + licenseText;
+
+	return merge(
+		gulp.src(paths.build+'/*.js')
+			.pipe(insert.prepend(commentString(headerText, "js"))),
+		gulp.src(paths.build+'/*.css')
+			.pipe(insert.prepend(commentString(headerText, "css"))),
+		gulp.src(paths.build+'/*.html')
+			.pipe(insert.prepend(commentString(headerText, "html")))
+	).pipe(gulp.dest(paths.build));
+
 });
+
+/* Helper functions
+ * ------------------------------------------------------------ */
+function commentString(string, fileType) {
+	var tags = {
+		html:	['<!--', '-->'],
+		css:	['/*', '*/'],
+		js:		['/*', '*/']
+	};
+	return tags[fileType][0]+' '+string+' '+tags[fileType][1];
+}
