263f45ca4548ade7cf82dd89322aa6662e68c3aa
lleaff <lleaff@users.noreply.github.com>
2015-05-30 00:16:24 +0200

    need to create a new object, otherwise the reference is direct

--- misc/arrayLikeObject.js
+++ misc/arrayLikeObject.js
@@ -1,8 +1,11 @@
 var Collection = function() {
 	this.hi = "HI";
 };
-Collection.prototype = Array.prototype;
+Collection.prototype = Object.create(Array.prototype);
 
 var col = new Collection();
 col.push("HELLO");
 console.log(col.hi, col.pop());
+
+Collection.prototype.testProp = "TEST";
+console.log(col.testProp, [].testProp);
