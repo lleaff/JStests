2afbe1586249327dd91679664c7b40926c64c537
lleaff <lleaff@users.noreply.github.com>
2015-11-23 20:17:59 +0100

    Use R.invoker() instead of custom methodN()

--- canvas/grass/grass.js
+++ canvas/grass/grass.js
@@ -19,32 +19,11 @@ function init() {
 
 var equalsW = R.curry((val1, val2) => val1 == val2);
 
-/**
- * Returns a function of the specified arity that calls an object's method.
- * @param {number} length - Arity of returned function, null to let R.curry
- *  detect it automatically.
- * @param {string} meth - Method name.
- * @param {object} obj - Object to call method on.
- * @return {function}
-*/
-var methodN = R.curry(function (length, meth, obj) {
-  return (length !== null ? R.curryN(length) : R.curry)(
-    function() {
-      return obj[meth].apply(obj, arguments);
-    });
-});
+var getCss = R.invoker(1, 'css');
+var setCss = R.invoker(2, 'css');
 
-var method = methodN(null);
-
-var getAttr = R.curry((prop, obj) =>
-                R.compose(R.flip(R.apply)([prop]), method('attr'))(obj));
-var setAttr = R.curry((prop, val, obj) =>
-                R.compose(R.flip(R.apply)([prop, val]), method('attr'))(obj));
-
-var getCss = R.curry((prop, obj) =>
-                R.compose(R.flip(R.apply)([prop]), method('css'))(obj));
-var setCss = R.curry((prop, val, obj) =>
-                R.compose(R.flip(R.apply)([prop, val]), method('css'))(obj));
+var getAttr = R.invoker(1, 'attr');
+var setAttr = R.invoker(2, 'attr');
 
 var width = getCss('width');
 
