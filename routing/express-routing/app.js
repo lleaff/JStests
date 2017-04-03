311e6df3f8b56027b64c6c4cd9729b16d56afab9
lleaff <lleaff@outlook.com>
2017-04-03 11:36:36 +0200

    Use res.locals and res.send override to log route

--- routing/express-routing/app.js
+++ routing/express-routing/app.js
@@ -5,25 +5,54 @@ app.get('/favicon.ico', (req, res) => {
   res.send('');
 })
 
+app.route = [];
+
+const { logRoute, logUse } = (() => {
+  function _logRouting(type, res, route) {
+    const log = `${type}(${route})`;
+    res.locals.route = [
+      ...(Array.isArray(res.locals.route) ?  res.locals.route : []),
+      log
+    ];
+  }
+  return {
+    logRoute(route) {
+      return _logRouting('route', this, route);
+    },
+    logUse(route) {
+      return _logRouting('use', this, route);
+    }
+  };
+})();
+
 app.use('*', (req, res, next) => {
-  console.log(`------------------------------
-    originalUrl: ${req.originalUrl}
-    referer: ${req.headers.referer}`);
+  const oldSend = res.send;
+  res.send = function patchedSend(...args) {
+    console.log('route: ', res.locals.route.map(r => `${r}`).join(' -> '));
+    return oldSend.call(this, ...args);
+  }
+
+  res.logRoute = logRoute;
+  res.logUse = logUse;
+
+  console.log(`------------------------------`);
+  console.log(`originalUrl: "${req.originalUrl}"`);
+  console.log(`referer: ${req.headers.referer}`);
   next();
 });
 
 app.use('/foobar', (req, res, next) => {
-  console.log(`use: /foobar`);
+  res.logUse('/foobar');
   next();
 });
 
 app.get('/foobar', (req, res, next) => {
-  console.log(`route: /foobar`);
+  res.logRoute('/foobar');
   next();
 });
 
 app.get('*', (req, res, next) => {
-  console.log(`route: *`);
+  res.logRoute('*');
   res.send('nothing');
 });
 
